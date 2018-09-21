7464741233e328e4b5370882273d9510a429840b
Marcus Willock <crazcalm@gmail.com>
2018-09-21 12:35:57 -0400

    Add README with instructions on how to add pokemon site
    The pokemon site needs to be added via hard simlinks
    from the my-pokemon-website project to here.
    
    I also added port flag with the default set to :8080

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # personal-site
 My Personal Website
+
+## Setup (Required)
+- Create hard simlinks from the my-pokemon-website to this repo.
+  - fe/pokemon_site.html ---> templates/pokemon_site.html
+  - fe/static/js/pokemon_site.js ---> static/js/pokemon_site.js
+  - fe/static/pokemon_site.css ---> static/css/pokemon_site.css
--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -8,8 +9,28 @@ import (
 	"path/filepath"
 )
 
+const (
+	portCMDDoc = "port to be used (Example: ':8080')"
+)
+
+var (
+	port string
+)
+
+func init() {
+	flag.StringVar(&port, "port", ":8080", portCMDDoc)
+	flag.StringVar(&port, "p", ":8080", portCMDDoc)
+}
+
 func main() {
+	flag.Parse()
+
 	indexPage, err := ioutil.ReadFile(filepath.Join(".", "templates", "personal_website.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pokemonPage, err := ioutil.ReadFile(filepath.Join(".", "templates", "pokemon_site.html"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,8 +47,20 @@ func main() {
 			log.Fatal(err)
 		}
 	})
+
+	mux.HandleFunc("/pokemon", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("pokemonPage").Parse(string(pokemonPage))
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(filepath.Join(".", "static")))))
 
-	server := http.Server{Addr: ":8081", Handler: mux}
+	server := http.Server{Addr: port, Handler: mux}
 	log.Fatal(server.ListenAndServe())
 }
