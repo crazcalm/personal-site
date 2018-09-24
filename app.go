4f9e98c3bab242a43f5e4b1e7710c5c53248aba5
Marcus Willock <crazcalm@gmail.com>
2018-09-24 13:33:35 -0400

    Create handler functions for the pages

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -14,7 +15,8 @@ const (
 )
 
 var (
-	port string
+	port   string
+	logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Llongfile)
 )
 
 func init() {
@@ -22,43 +24,43 @@ func init() {
 	flag.StringVar(&port, "p", ":8080", portCMDDoc)
 }
 
+type page struct {
+	name         string
+	templatePage string
+	log          *log.Logger
+}
+
+func (p page) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New(p.name).Parse(string(p.templatePage))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.log.Printf("Serving %s -- view count: %d", p.name)
+}
+
 func main() {
 	flag.Parse()
 
-	indexPage, err := ioutil.ReadFile(filepath.Join(".", "templates", "personal_website.html"))
+	indexPagePath, err := ioutil.ReadFile(filepath.Join(".", "templates", "personal_website.html"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	indexPage := page{name: "indexPage", templatePage: string(indexPagePath), log: logger}
 
-	pokemonPage, err := ioutil.ReadFile(filepath.Join(".", "templates", "pokemon_site.html"))
+	pokemonPagePath, err := ioutil.ReadFile(filepath.Join(".", "templates", "pokemon_site.html"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	pokemonPage := page{name: "pokemonPage", templatePage: string(pokemonPagePath), log: logger}
 
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.New("indexPage").Parse(string(indexPage))
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = t.Execute(w, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	mux.HandleFunc("/pokemon", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.New("pokemonPage").Parse(string(pokemonPage))
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = t.Execute(w, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
+	mux.Handle("/", indexPage)
+	mux.Handle("/pokemon", pokemonPage)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(filepath.Join(".", "static")))))
 
 	server := http.Server{Addr: port, Handler: mux}
